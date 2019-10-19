ec6e94993d60e9c8bd3797c705a0c8b1161ec7f1
haroldo <haroldoop@gmail.com>
2019-10-18 22:29:18 -0300

    Minor map generation cleanup.

--- map-generator.ts
+++ map-generator.ts
@@ -5,69 +5,57 @@ import SimplexNoise from 'simplex-noise';
 
 const simplex = new SimplexNoise();
 
-const step1Tiles = [];
 const floorTiles = [50, 51, 60, 61,62, 63];
 const floorTiles2 = [16, 46];
 
-const generateTileIndexesStep1 = (tileCount = 10) => {
-	const position = step1Tiles.length;
-	const indexes = range(tileCount).map((j) => simplex.noise2D(position, j) > 0.5 ? 1 : 0;
-	step1Tiles.push(indexes);
-
-	return indexes;
+const generateTileIndexesStep1 = (position, {tileCount = 10} = {}) => {
+	return range(tileCount).map((j) => simplex.noise2D(position, j) > 0.5 ? 1 : 0;
 };
 
-const generateTileIndexes = (tileCount = 10) => {
-	generateTileIndexesStep1();
-
-	const row = step1Tiles.length - 2;
-	const prevStrip = step1Tiles[row - 1];
-	const strip = step1Tiles[row];
-	const nextStrip = step1Tiles[row + 1];
-
-	return strip.map((idx, col) => { 
-		if (!idx && col > 0 && col < strip.length -1) {
+const generateTileIndexes = (bottom, current, top, {tileCount = 10} = {}) => {
+	return current.map((idx, col) => { 
+		if (!idx && col > 0 && col < current.length -1) {
 			// Left edge
-			if (strip[col - 1]) {
-				return prevStrip[col] ? 5 
-					: nextStrip[col] ? 25
+			if (current[col - 1]) {
+				return bottom[col] ? 5 
+					: top[col] ? 25
 					: 15;
 			}
 
 			// Right edge
-			if (strip[col + 1]) {
-				return prevStrip[col] ? 7
-					: nextStrip[col] ? 27
+			if (current[col + 1]) {
+				return bottom[col] ? 7
+					: top[col] ? 27
 					: 17;
 			}
 
 			// Bottom edge
-			if (prevStrip[col]) {
+			if (bottom[col]) {
 				return 26;
 			}
 
 			// Top edge
-			if (nextStrip[col]) {
+			if (top[col]) {
 				return 6;
 			}
 
 			// Bottom left corner
-			if (prevStrip[col - 1]) {
+			if (bottom[col - 1]) {
 				return 18;
 			}
 
 			// Bottom right corner
-			if (prevStrip[col + 1]) {
+			if (bottom[col + 1]) {
 				return 19;
 			}
 
 			// Top left corner
-			if (nextStrip[col - 1]) {
+			if (top[col - 1]) {
 				return 8;
 			}
 
 			// Top right corner
-			if (nextStrip[col + 1]) {
+			if (top[col + 1]) {
 				return 9;
 			}
 		}
@@ -76,8 +64,15 @@ const generateTileIndexes = (tileCount = 10) => {
 	});
 };
 
-for (let i = 0; i != 3; i++) {			
-	generateTileIndexesStep1();
-}
-
-export const mapGenerator = () => generateTileIndexes;
+export const mapGenerator = () => {
+	let position = 0;
+	
+	let bottom;
+	let current = generateTileIndexesStep1(position++);
+	let top = generateTileIndexesStep1(position++);
+	
+	return () => {
+		[bottom, current, top] = [current, top, generateTileIndexesStep1(position++)];		
+		return generateTileIndexes(bottom, current, top);
+	}
+};
